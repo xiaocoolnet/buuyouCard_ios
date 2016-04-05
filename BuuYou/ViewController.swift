6cc77c5e2a462d5c344e11aa1c8c46b19f6c7476
niuyao <490393730@qq.com>
2016-04-05 16:18:26 +0800

    Initial Commit

---
+++ BuuYou/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BuuYou
+//
+//  Created by Mac on 16/4/5.
+//  Copyright © 2016年 北京校酷网络科技有限公司. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

d47c495212519ff9840af93679554ed35df97a94
niuyao <490393730@qq.com>
2016-04-07 09:22:46 +0800

    2016年4月7日，创建布优销卡iOS端工程。

---
+++ Mine/Controller/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  BuuYou
+//
+//  Created by niu on 16/4/5.
+//  Copyright © 2016年 北京校酷网络科技有限公司. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
